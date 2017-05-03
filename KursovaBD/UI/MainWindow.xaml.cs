a68d6b1f00c37c77b33fb1618940e9a079245664
LeeRising <leerising773@gmail.com>
2017-05-03 14:10:28 +0300

    Add favicon

--- KursovaBD/UI/MainWindow.xaml.cs
+++ KursovaBD/UI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -19,8 +20,6 @@ namespace KursovaBD
         public MainWindow()
         {
             InitializeComponent();
-            btn.Click += delegate {
-            };
         }
     }
 }
