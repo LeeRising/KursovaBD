f90aad5fdb9e0d6a811f18ec794568b65a6f3a8d
LeeRising <leerising773@gmail.com>
2017-05-06 10:49:36 +0300

    Binding to Badge

--- KursovaBD/UI/MainWindow.xaml.cs
+++ KursovaBD/UI/MainWindow.xaml.cs
@@ -25,9 +25,11 @@ namespace KursovaBD
         Connector MySqlConnector = new Connector();
         SnackbarMessageQueue messageQueue;
         MySqlCommand msc;
+        public int Count => 6;
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
             messageQueue = MessagesSnackbar.MessageQueue;
             #region TitleBar
             CloseBtn.Click += delegate
@@ -52,18 +54,19 @@ namespace KursovaBD
             {
 
             };
+            CheckRequests();
         }
         void CheckRequests()
         {
-            MySqlConnection mc = MySqlConnector.MySqlConnectionMethod();
-            mc.Open();
-            msc = new MySqlCommand("select count(id) from requests",mc);
-            var v = int.Parse(msc.ExecuteScalar().ToString());
-            if (v != 0)
-                CountingRequestBadge.Badge = (object)v;
-            else
-                CountingRequestBadge.Badge = new Binding("0");
-            mc.Close();
+            //MySqlConnection mc = MySqlConnector.MySqlConnectionMethod();
+            //mc.Open();
+            //msc = new MySqlCommand("select count(id) from requests",mc);
+            //var v = int.Parse(msc.ExecuteScalar().ToString());
+            //if (v != 0)
+            //    CountingRequestBadge.Badge = (object)v;
+            //else
+            //    CountingRequestBadge.Badge = null;
+            //mc.Close();
         }
 
         private void LoginDialog_DialogClosing(object sender, DialogClosingEventArgs eventArgs)
@@ -76,12 +79,9 @@ namespace KursovaBD
                     LoginTb.Text, Cryptography.getHashSha256(PassTb.Password)), mc);
                 if (msc.ExecuteScalar() != null)
                 {
-                    Dispatcher.Invoke(()=>
-                    {
                         Task.Factory.StartNew(() => messageQueue.Enqueue("Hello " + LoginTb.Text));
                         LoginBtn.Content = "Hello " + LoginTb.Text;
                         CountingRequestBadge.Visibility = Visibility.Visible;
-                    });
                 }
             }
         }
