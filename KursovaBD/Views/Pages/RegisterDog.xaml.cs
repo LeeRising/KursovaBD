a9e6c893bff3520b16b08ec4b5e23b06ed316a0c
LeeRising <leerising773@gmail.com>
2017-05-12 00:39:09 +0300

    UPD Dog Register

--- KursovaBD/Views/Pages/RegisterDog.xaml.cs
+++ KursovaBD/Views/Pages/RegisterDog.xaml.cs
@@ -9,6 +9,8 @@ using Microsoft.Win32;
 using System.Windows.Media.Imaging;
 using System.Threading.Tasks;
 using KursovaBD.Models;
+using System.IO;
+using System.Net;
 
 namespace KursovaBD.UI.Pages
 {
@@ -23,7 +25,7 @@ namespace KursovaBD.UI.Pages
             Filter= "Image Files (*.bmp, *.jpg ,*.png)|*.bmp;*.jpg;*.png",
             FileName="Chose dog picture"
         };
-        string avatar="as";
+        string avatar = "No_image.png",avatar1;
         public RegisterDog()
         {
             InitializeComponent();
@@ -36,6 +38,7 @@ namespace KursovaBD.UI.Pages
                 {
                     DogPhoto.Source = new BitmapImage(new Uri(_OpenFileDialog.FileName));
                     avatar = _OpenFileDialog.FileName;
+                    avatar1 = avatar;
                 }
                 catch (Exception)
                 {
@@ -61,11 +64,36 @@ namespace KursovaBD.UI.Pages
                 using (DbConnection)
                 {
                     DbConnection.Open();
+                    msc = new MySqlCommand("select count(id) from dogs",DbConnection);
+                    var _id = Int64.Parse(msc.ExecuteScalar().ToString());
+                    avatar = avatar != "No_image.png" ? (_id==0?"1":(_id+=1).ToString()) + "." + avatar.Split('.')[1] : "No_image.png";
                     msc = new MySqlCommand(String.Format("insert into dogs (Club_id,Name,Breed,Age,Document_info,Parents_name,Date_last_vaccenation,Master_id,Photo,Request) " +
                         "values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','waiting')",
                         ClubComboBox.SelectedIndex+1,NameTb.Text,BreadComboBox.SelectedValue,AgeTb.Text,DocumentInfoTb.Text,ParentsnameTb.Text, LastVacDate.DisplayDate.Date.ToString("yyyy-MM-dd"),MasterComboBox.SelectedIndex+1,avatar), DbConnection);
-                    msc.ExecuteScalar();
+                    msc.ExecuteNonQuery();
+                    if (!String.IsNullOrEmpty(avatar1))
+                    {
+                        File.Copy(avatar1, Path.Combine(AppDomain.CurrentDomain.BaseDirectory + @"\") + avatar);
+                        FtpWebRequest request = FtpWebRequest.Create("ftp://kursova.sytes.net/" + avatar) as FtpWebRequest;
+                        request.Method = WebRequestMethods.Ftp.UploadFile;
+                        request.Credentials = new NetworkCredential("ftp", "ftp");
+                        request.UsePassive = true;
+                        request.UseBinary = true;
+                        request.KeepAlive = false;
+                        FileStream stream = File.OpenRead(avatar);
+                        byte[] buffer = new byte[stream.Length];
+                        stream.Read(buffer, 0, buffer.Length);
+                        stream.Close();
+                        Stream reqStream = request.GetRequestStream();
+                        reqStream.Write(buffer, 0, buffer.Length);
+                        reqStream.Close();
+                        request.Abort();
+                        File.Delete(avatar);
+                    }
                 }
+                Task.Factory.StartNew(() => messageQueue.Enqueue("New dog has been registred!"));
+                MainWindow.Instance.SetDefaultContent();
+                MainWindow.Instance.DogRegisterBtn.Style = FindResource("MaterialDesignRaisedButton") as Style;
             }
             else
                 Task.Factory.StartNew(() => messageQueue.Enqueue("All fields must be not empty!"));
